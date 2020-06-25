f3f031d5976a6587ae1ba05010215ca2349f56e6
Dexter <dfzxyz@gmail.com>
2020-06-25 16:22:09 +1000

    which

--- helloworld/homepage.py
+++ helloworld/homepage.py
@@ -1,1 +1,11 @@
 print('Hello Team, this is the EASY CRM')
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return 'Hello, World!'
+
+print ('4')
+
+#add description
